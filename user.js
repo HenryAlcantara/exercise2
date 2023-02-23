bcb0e25ee4a2f45046cb4a6cd5592b5e6e7eb57d
HenryAlcantara <smeliodas913@gmail.com>
2023-02-23 23:36:20 +0800

    feat: added a txt file, added dependencies in the package, created saveUser function in user.js

--- user.js
+++ user.js
@@ -1,3 +1,11 @@
 //imports fs and unique-string packages
 import fs from 'fs';
 import uniqueString from 'unique-string';
+
+//function for saving user information
+function saveUser(firstname, lastname, email, age){
+    if(firstname.length != 0 && lastname.length != 0 && email.length != 0 && age >= 18){
+        fs.appendFileSync("user.txt", firstname+","+lastname+","+email+","+age+","+uniqueString().substring(0, 10)+"\n");
+    }
+}
+

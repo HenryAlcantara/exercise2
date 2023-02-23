4384640784b8b2d647fc0e305a0b6786de9d76d2
HenryAlcantara <smeliodas913@gmail.com>
2023-02-23 23:48:00 +0800

    test: added test cases and tried to export-import the saveUser function in user.js

---
+++ main.js
@@ -0,0 +1,6 @@
+import saveUser from './user.js';
+
+saveUser("Hermiony", "Granger", "hg@gmail.com", 19);
+saveUser("Agatha", "Timothy", "at@gmail.com", 19);
+saveUser("Stewkesburg", "William", "sw@gmail.com", 19);
+saveUser("Harry", "Potter", "hp@gmail.com", 19);
--- user.js
+++ user.js
@@ -5,7 +5,10 @@ import uniqueString from 'unique-string';
 //function for saving user information
 function saveUser(firstname, lastname, email, age){
     if(firstname.length != 0 && lastname.length != 0 && email.length != 0 && age >= 18){
-        fs.appendFileSync("user.txt", firstname+","+lastname+","+email+","+age+","+uniqueString().substring(0, 10)+"\n");
+        fs.appendFileSync("users.txt", firstname+","+lastname+","+email+","+age+","+uniqueString().substring(0, 10)+"\n");
+    }else{
+        console.log("error.")
     }
 }
 
+export default saveUser
